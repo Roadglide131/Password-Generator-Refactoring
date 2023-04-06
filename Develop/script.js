98f40932aadb6aca144b6ec1a6228fce3e2a98fb
Roadglide131 <darrenj@oldschoolrenovation.com>
2023-04-06 17:58:59 -0500

    Write password to the #password input

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,13 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 
 }
 
